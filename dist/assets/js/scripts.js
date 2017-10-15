d92f6b4b567186c7937f601d3ece638518a737fe
hafizalfaza <hafizalfaza@gmail.com>
2017-10-15 16:19:12 +0800

    Fix scroll event reference

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -8,31 +8,53 @@ var flkty = new Flickity( elem, {
   wrapAround: true
 });
 
-var titleElem = document.querySelectorAll('.section-title');
+var sectionEl = document.querySelectorAll('.block-animate-common');
 
-var titleElArray = [];
+var sectionElArray = [];
 
-titleElem.forEach((el) => {
+sectionEl.forEach((el) => {
   var viewportOffset = el.getBoundingClientRect();
   var top = viewportOffset.y;
   var left = viewportOffset.x;
   var height = viewportOffset.height;
 
-  titleElArray.push({
+  sectionElArray.push({
     el,
     top,
-    height
+    height,
+    allowActivate: true
   })
 
 })
 
 window.onscroll = function(){
   const scrollTop = window.scrollY; 
-  titleElArray.forEach((titleEl) => {
-    if(scrollTop > titleEl.top - 400 && scrollTop < titleEl.top + titleEl.height) {
-      titleEl.el.classList.add('active')
-    }else{
-      titleEl.el.classList.remove('active')
+  sectionElArray.forEach((sectionEl) => {
+
+    const childrenEl = Array.from(sectionEl.el.children[0].children[0].children);
+
+    if(scrollTop >= sectionEl.top && sectionEl.allowActivate) {
+      childrenEl.forEach((el) => {
+        el.classList.add('active')
+        sectionEl.allowActivate = false
+      })
+    }
+    
+    if (scrollTop >= sectionEl.height + sectionEl.top){
+      childrenEl.forEach((el) => {
+        el.classList.remove('active')  
+      })
+    }
+
+    if(scrollTop < sectionEl.top+200) {
+      sectionEl.allowActivate = true
+    }
+
+    if((scrollTop < 50 +  (sectionEl.top - sectionEl.height)) || scrollTop < 50){
+      childrenEl.forEach((el) => {
+        el.classList.remove('active')
+      })
+      sectionEl.allowActivate = true
     }
   })
 }
